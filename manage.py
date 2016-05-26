ba1a53f368a53d2c2b6850a41931be43e5bec6ea
KyleRui0202 <ruiyang0202@gmail.com>
2016-05-25 22:59:24 -0400

    Update the manage.py according to new app structure

--- manage.py
+++ manage.py
@@ -1,4 +1,13 @@
-from flask_script import Manager
+"""
+    manage
+    ~~~~~~
+
+    This script provides ready-to-use commands for
+    * database migration
+    * running local development server
+"""
+
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 from acmewines import app, db
@@ -6,7 +15,14 @@ from acmewines import app, db
 migrate = Migrate(app, db)
 
 manager = Manager(app)
+
+from acmewines.models import Order
+
+# Migration commands 
 manager.add_command('db_migration', MigrateCommand)
+
+# Run local server
+manager.add_command('runserver', Server('localhost', port=8888))
 
 if __name__ == '__main__':
     manager.run()
