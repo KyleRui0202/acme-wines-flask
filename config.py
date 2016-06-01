7d08e5b965c4499261b4b3af211dadd400a26335
KyleRui0202 <ruiyang0202@gmail.com>
2016-06-01 12:54:26 -0400

    Update dependencies and config

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
-DEBUG = False
-SQLALCHEMY_DATABASE_URI = 'postgresql://rui:y650515R@localhost:5432/acmewines'
+"""
+    config
+    ~~~~~~
 
+    This is the configuration for acmewines
+"""
+
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'postgresql://rui:y650515R@localhost:5432/acmewines'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
