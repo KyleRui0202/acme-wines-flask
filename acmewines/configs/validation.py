067d30a4da4b3b0fef691772e8c978d8bd472f2d
KyleRui0202 <ruiyang0202@gmail.com>
2016-06-02 17:53:41 -0400

    Validate missing field and update validation logic when a field is corrected

--- acmewines/configs/validation.py
+++ acmewines/configs/validation.py
@@ -4,16 +4,18 @@
 
     This is the configuration for order validation
 """
+# Required fields for wine order
+required_fields = ('id', 'name', 'email', 'state', 'zipcode', 'birthday')
 
 # Valid U.S. state/area abbreviations for order
-states = [AL, AK, AZ, AR, CA, CO, CT, DE, DC, FL, GA, HI,
-    ID, IL, IN, IA, KS, KY, LA, ME, MD, MA, MI, MN, MS, MO,
-    MT, NE, NV, NH, NJ, NM, NY, NC, ND, OH, OK, OR, PA, RI,
-    SC, SD, TN, TX, UT, VT, VA, WA, WV, WI, WY, AS, GU, MP,
-    PR, VI, FM, MH, PW]
+states = ('AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA',
+    'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI',
+    'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND',
+    'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA',
+    'WA', 'WV', 'WI', 'WY', 'AS', 'GU', 'MP', 'PR', 'VI', 'FM', 'MH', 'PW')
 
 # The States that we don't ship wines to
-states_not_allowed = [NJ, CT, PA, MA, IL, ID, OR]
+states_not_allowed = ('NJ', 'CT', 'PA', 'MA', 'IL', 'ID', 'OR')
 
 # Valid zipcode pattern in regular expression 
 zipcode_pattern = '^\d{5}([\-]\d{4})?$'
@@ -26,3 +28,5 @@ birthday_format = '%b %d, %Y'
 
 # The minimum age of the orderer
 min_age = 21
+
+
--- acmewines/models.py
+++ acmewines/models.py
@@ -1,5 +1,6 @@
-from datetime import datetime, date
+import re
 
+from datetime import datetime, date
 from sqlalchemy.dialects.postgresql import JSON as PSQLJSON
 
 from flask import json
@@ -7,7 +8,7 @@ from flask import json
 from acmewines import db
 
 class Order(db.Model):
-    __tablename__ = 'order'
+    __tablename__ = 'orders'
     __mapper_args__ = {
         'exclude_properties': ['created_at', 'updated_at']
     }
@@ -15,18 +16,21 @@ class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     name_column = db.Column('name', db.String(128), nullable=True, index=True)
     email_column = db.Column('email', db.String(254), nullable=True, index=True)
-    state_column = db.Column('state', db.String(2), nullable=True)
-    zipcode_column = db.Column('zipcode', db.String(10), nullable=True)
+    state_column = db.Column('state', db.String(30), nullable=True)
+    zipcode_column = db.Column('zipcode', db.String(20), nullable=True)
     birthday_column = db.Column('birthday', db.Date, nullable=True)
     valid = db.Column(db.Boolean, nullable=True)
     validation_failure = db.Column(PSQLJSON, nullable=True)
     created_at = db.Column(db.DateTime, server_default=db.text('NOW()'))
     updated_at = db.Column(db.DateTime, server_default=db.text('NOW()'),
         onupdate=datetime.now)
-    ix_state_zipcode = db.Index('ix_state_zipcode', state_column, zipcode_column)
+    ix_state_zipcode = db.Index('ix_orders_state_zipcode', state_column, zipcode_column)
 
     _visible = ('id', 'name', 'email', 'state', 'zipcode', 'birthday',
         'valid', 'validation_failure')
+
+    from acmewines.configs.validation import required_fields
+    _required = required_fields
 
     # Properties
     @property
@@ -76,7 +80,10 @@ class Order(db.Model):
     @property
     def birthday(self):
         """Get the birthday of the orderer"""
-        return self.birthday_column.isoformat()
+        if self.birthday_column:
+            return self.birthday_column.isoformat()
+        else:
+            return None
 
     @birthday.setter
     def birthday(self, value):
@@ -87,7 +94,7 @@ class Order(db.Model):
     def __init__(self, id, name=None, email=None,
         state=None, zipcode=None, birthday=None):
         self.id = id
-        self.valid = None
+        self.valid = True
         self.validation_failure = None
 
         if name:
@@ -108,89 +115,106 @@ class Order(db.Model):
         if validation_errors:
             if self.validation_failure is None:
                 self.validation_failure = {}
-            self.validation_failure.update(validation_errors)
-            if self.valid or self.valid is None:
-                self.valid = False
+            for error_name in validation_errors:
+                if validation_errors[error_name]:
+                    self.validation_failure[error_name] =\
+                        validation_errors[error_name]
+                elif error_name in self.validation_failure:
+                    del self.validation_failure[error_name]
+            if self.validation_failure:
+                if self.valid != False:
+                    self.valid = False
+            else:
+                self.validation_failure = None
+                if self.valid != True:
+                    self.valid = True
 
-    def _parse_required_field(self, field, value):
-        is_valid = True
-
-        if value:
-            setattr(self, field, value)
-            field_validation_method = getattr(self, '_validate_' + field)
-            validation_errors = field_validation_method(value)
-            if valid_errors:
-                is_valid = False
-                self.validation_failure.update(validation_errors)
-        else:
-            is_valid = False
-            self.validation_failure['required_'+field] = 'The ' + field +\
-                ' is missing'
-
-        if self.valid and not is_valid:
-            self.valid = False
-        
+    def _validate_missing_fields(self):
+        validation_errors = {}
+        for field in Order._required:
+            if getattr(self, field, None) is None:
+                validation_errors['required_'+field] =\
+                    'The %s is missing' % field
+        return validation_errors or None
+     
     def _validate_name(self, value):
-        return None 
+        return None
 
     def _validate_email(self, value):
+        email_errors = {'email_validation': None}
         from validate_email import validate_email
         is_valid = validate_email(value)
-        if is_valid:
-            return None
-        else:
-            return {'email_validation': 'The email address is not valid'}
+        if not is_valid:
+            email_errors['email_validation'] =\
+                'The email address is not valid'
+        return email_errors
 
     def _validate_state(self, value):
-        from acmewines.configs.validation import states, states_not_allowed
+        state_errors = {'state_validation': None,
+            'allowed_states': None}
+        from acmewines.configs.validation import (
+            states, states_not_allowed)
         if value in states:
             if value in states_not_allowed:
-                return {'allowed_states': "We don't ship to " + value}
-            else:
-                return None
+                state_errors['allowed_states'] =\
+                    "We don't ship to " + value
         else:
-            return {'state_validation': value + ' is not a valid/allowed' +\
-                ' U.S. state abbreviation'}
+            state_errors['state_validation'] = value +\
+                ' is not a valid/allowed U.S. state abbreviation'
+        return state_errors
 
     def _validate_zipcode(self, value):
-        from acmewines.configs.validation import zipcode_pattern, zipcode_max_digit_sum
-        is_valid = re.match('\d{5}([\-]\d{4})?$', value)
+        zipcode_errors = {'zipcode_validation': None,
+            'zipcode_digit_sum': None}
+        from acmewines.configs.validation import (
+            zipcode_pattern, zipcode_max_digit_sum)
+        is_valid = re.match(zipcode_pattern, value)
         if is_valid:
             zipcode_digit_sum = 0
-            for digit in digit_only_zipcode:
+            for digit in value:
                 if digit != '-':
                     zipcode_digit_sum += int(digit)
             if zipcode_digit_sum > zipcode_max_digit_sum:
-                return {'zip_code_sum': 'Your zipcode is too large (>' +\
-                    zipcode_max_digit_sum + ')'}
-            else:
-                return None
+                zipcode_errors['zipcode_digit_sum'] =\
+                    "The sum of zipcode's digits " +\
+                    'is too large (> %d)' % zipcode_max_digit_sum
         else:
-            return {'zipcode_validation': value + ' is not a valid' +\
-                '5-digit zipcode (e.g., 00000) or 9-digit zipcode (e.g., 00000-0000)'}
+            zipcode_errors['zipcode_validation'] = value + ' is not a valid' +\
+                '5-digit (e.g., 00000) or 9-digit (e.g., 00000-0000) zipcode'
+        return zipcode_errors
 
     def _parse_and_validate_birthday(self, value):
         parsed_birthday = None
-        validation_errors = None
-        from acmewines.configs.validation import birthday_format, min_age
+        birthday_errors = {'birthday_validation': None,
+            'age_restriction': None}
+        from acmewines.configs.validation import (
+            birthday_format, min_age)
         try:
-             parsed_birthday = datetime.strptime(value, birthday_format).date()
+            parsed_birthday = datetime.strptime(value, birthday_format).date()
         except ValueError:
-             pass
+            pass
         if parsed_birthday:
             today = date.today()
             max_birth_date = date(today.year-21, today.month, today.day)
             if parsed_birthday > max_birth_date:
-                validation_errors = {'age_restriction': 'You must be ' +\
-                    min_age + ' or older to order'}
+                birthday_errors['age_restriction'] =\
+                    'You must be %d or older to order' % min_age
         else:
-            validation_errors = {'birtday_validation': value +\
-                ' is not a valid birthday format: ' + birthday_format}
-        return (parsed_birthday, validation_errors)
+            birthday_errors['birtday_validation'] =\
+                '%s is not a valid birthday format: %s' %\
+                (value, birthday_format)
+        return parsed_birthday, birthday_errors
 
     def toDict(self):
         fieldDict = {}
         for field_name in Order._visible:
-            if hasattr(self, field_name):
-                fieldDict[field_name] = getattr(self, field_name)
+            field_value = getattr(self, field_name, None)
+            if field_value is not None:
+                fieldDict[field_name] = field_value
         return fieldDict
+
+    def save(self):
+        missing_field_errors = self._validate_missing_fields()
+        self._update_validation_failure(missing_field_errors)  
+        db.session.add(self)
+        db.session.commit()
